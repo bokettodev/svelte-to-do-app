de495f51bcbd43869085f04bfeaae0a86858600f
Raphael Jancherov <bokettodev@gmail.com>
2022-03-17 22:03:12 +0300

    chore: docs rebuild

---
+++ docs/_app/pages/__layout.svelte-e68bb3a5.js
@@ -0,0 +1,49 @@
+import{S as m,i as f,s as u,F as h,e as g,t as y,k as _,G as b,c as v,a as w,h as $,d as c,m as x,b as z,H as p,g as R,I as j,J as k,K as q,q as E,o as S}from"../chunks/vendor-de5c08b0.js";function T(s){let o,i,r,e;const l=s[1].default,t=h(l,s,s[0],null);return{c(){o=g("style"),i=y(`@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+:root {
+  --bg-primary-color: #3b3745;
+  --bg-secondary-color: #24222e;
+  --text-primary-color: white;
+  --text-secondary-color: gray;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: var(--bg-primary-color);
+  font-family: "Roboto", arial, helvetica, sans-serif;
+  height: 100vh;
+  width: 100vw;
+}
+
+input {
+  font-size: 1rem;
+  outline: none;
+}`),r=_(),t&&t.c(),this.h()},l(n){const a=b('[data-svelte="svelte-jjzje0"]',document.head);o=v(a,"STYLE",{lang:!0});var d=w(o);i=$(d,`@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+:root {
+  --bg-primary-color: #3b3745;
+  --bg-secondary-color: #24222e;
+  --text-primary-color: white;
+  --text-secondary-color: gray;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: var(--bg-primary-color);
+  font-family: "Roboto", arial, helvetica, sans-serif;
+  height: 100vh;
+  width: 100vw;
+}
+
+input {
+  font-size: 1rem;
+  outline: none;
+}`),d.forEach(c),a.forEach(c),r=x(n),t&&t.l(n),this.h()},h(){document.title="ToDoApp",z(o,"lang","scss")},m(n,a){p(document.head,o),p(o,i),R(n,r,a),t&&t.m(n,a),e=!0},p(n,[a]){t&&t.p&&(!e||a&1)&&j(t,l,n,n[0],e?q(l,n[0],a,null):k(n[0]),null)},i(n){e||(E(t,n),e=!0)},o(n){S(t,n),e=!1},d(n){c(o),n&&c(r),t&&t.d(n)}}}function A(s,o,i){let{$$slots:r={},$$scope:e}=o;return s.$$set=l=>{"$$scope"in l&&i(0,e=l.$$scope)},[e,r]}class D extends m{constructor(o){super();f(this,o,A,T,u,{})}}export{D as default};
