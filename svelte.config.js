ea25faa28cb555f19c4fdc6167006fcb521e9ae3
Raphael Jancherov <bokettodev@gmail.com>
2022-03-21 02:58:37 +0300

    fix: svelte.config improved

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = 'production' === 'development';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,11 +9,15 @@ const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
-			assets: 'docs'
+			assets: 'docs',
+			fallback: null,
+			precompress: false
 		}),
 		paths: {
-			// change below to your repo name
-			base: dev ? "''" : '/svelte-to-do-app'
+			base: dev ? '' : '/svelte-to-do-app'
+		},
+		prerender: {
+			default: true
 		}
 	}
 };
